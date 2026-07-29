6e142e2651adffc7e61d8ef06137aacd4bfccf17
Luvcie <lucpardo@student.42.fr>
2026-07-29 13:49:07 +0200

    message: being able to send messages with PRIVMSG to a user or a channel
    The target is the first parameter. If it starts with # it's a channel,
    so the message goes to everyone in the channel except the person who
    sent it (403 if the channel doesn't exist, 404 if the sender isn't in
    it). Otherwise the target is a nickname and the message goes to that
    one person, 401 if they aren't connected.
    
    Even though it's called PRIVMSG, it's used for both private messages
    and normal public channel messages. That's just how IRC works even if
    the name sounds confusing.

--- srcs/commands/cmd_message.cpp
+++ srcs/commands/cmd_message.cpp
@@ -13,5 +13,30 @@ void Server::cmdPrivmsg(Client& client, const Message& msg) {
         sendNumeric(client, "412", ":No text to send");
         return;
     }
-    // TODO: in the next commit send it to a nick or to a channel
+
+    const std::string& target = msg.params[0];
+    const std::string& text = msg.params[1];
+    std::string line = clientPrefix(client) + " PRIVMSG " + target + " :" + text + "\r\n";
+
+    // a target starting with # is a channel, anything else is a nickname
+    if (!target.empty() && target[0] == '#') {
+        std::map<std::string, Channel>::iterator it = _channels.find(target);
+        if (it == _channels.end()) {
+            sendNumeric(client, "403", target + " :No such channel");
+            return;
+        }
+        if (!it->second.members.count(client.fd)) {
+            sendNumeric(client, "404", target + " :Cannot send to channel");
+            return;
+        }
+        // to everyone in the channel but the one who sent it
+        sendToChannel(target, line, client.fd);
+    } else {
+        Client* dest = findClientByNick(target);
+        if (!dest) {
+            sendNumeric(client, "401", target + " :No such nick");
+            return;
+        }
+        sendToClient(dest->fd, line);
+    }
 }
